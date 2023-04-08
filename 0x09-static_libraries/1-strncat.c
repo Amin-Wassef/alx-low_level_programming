a68a113fa764876af52f19ca083eb15e1c73b675
Amin-Wassef <dr.amin.wassef@gmail.com>
2023-04-08 04:50:49 -0700

    Done

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -2,11 +2,9 @@
 
 /**
  * _strncat - concatenate strings, defining the size of second string.
- 
  * @dest: string with concatenation
  * @src: string to be concatenated
  * @n: size of second string
- 
  * Return: Always 0.
  */
 
